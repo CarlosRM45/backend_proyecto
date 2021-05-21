552dbf726d15851d4af3deb2d28ef1d23a9e58a2
martel.royss21 <martel.royss21@gmail.com>
2021-05-21 01:23:02 -0500

    se agrego servicio para marcas

---
+++ backend/src/main/java/com/gustilandia/backend/repository/MarcaRepository.java
@@ -0,0 +1,17 @@
+package com.gustilandia.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.query.Param;
+import com.gustilandia.backend.model.Cliente;
+import com.gustilandia.backend.model.Marca;
+
+public interface MarcaRepository extends JpaRepository<Marca, Long>{
+
+    // @Transactional
+    // @Modifying(clearAutomatically = true)
+    // @Query(value = "update marca set id_estado = 2 where id_marca =:id",nativeQuery = true)
+    // int deleteMarca(@Param("id") Long id);
+}
