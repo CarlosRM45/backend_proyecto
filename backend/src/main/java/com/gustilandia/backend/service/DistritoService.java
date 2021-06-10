6119b881d798c7455afcbf51e5d438a1f03c5969
Carlos <Carlos@LAPTOP-QH2A519H>
2021-06-09 20:01:27 -0500

    Listar Distritos

---
+++ backend/src/main/java/com/gustilandia/backend/service/DistritoService.java
@@ -0,0 +1,11 @@
+package com.gustilandia.backend.service;
+
+import java.util.List;
+
+import com.gustilandia.backend.dto.DTODistrito;
+
+public interface DistritoService {
+	
+	List<DTODistrito> listarDistritos();
+
+}
