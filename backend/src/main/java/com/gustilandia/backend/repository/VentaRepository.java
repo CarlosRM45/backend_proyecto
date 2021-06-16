bdccf64535f6bc89e0f561c5c7d661de01aa26f6
marcomiler <miler.rmarco@gmail.com>
2021-06-16 01:08:58 -0500

    Cambio en la repo de Venta

--- backend/src/main/java/com/gustilandia/backend/repository/VentaRepository.java
+++ backend/src/main/java/com/gustilandia/backend/repository/VentaRepository.java
@@ -8,24 +8,25 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.repository.query.Param;
 
-public interface VentaRepository extends JpaRepository<Venta, Long>{
+public interface VentaRepository extends JpaRepository<Venta, Long> {
 
     @Query(value = "exec dbo.sp_insertVenta :idcliente, :idproducto ", nativeQuery = true)
     Long insertVentaCarrito(@Param("idcliente") Long idcliente, @Param("idproducto") Long idproducto);
 
     @Query(value = "exec dbo.sp_aumentarCantidadProducto :idproducto, :idcliente, :cantidad ", nativeQuery = true)
-    Long aumentarCantidadProducto(@Param("idcliente") Long idcliente, @Param("idproducto") Long idproducto, @Param("cantidad") int cantidad);
+    Long aumentarCantidadProducto(@Param("idcliente") Long idcliente, @Param("idproducto") Long idproducto,
+            @Param("cantidad") int cantidad);
 
     @Query(value = "exec dbo.cambiarEstadoVenta :idVenta, :idRepartidor, :idEstado ", nativeQuery = true)
-    Long cambiarEstadoVenta(@Param("idVenta") Long idVenta, @Param("idRepartidor") Long idRepartidor, @Param("idEstado") Long idEstado);
-
+    Long cambiarEstadoVenta(@Param("idVenta") Long idVenta, @Param("idRepartidor") Long idRepartidor,
+            @Param("idEstado") Long idEstado);
 
     @Transactional
     @Modifying(clearAutomatically = true)
-    @Query(value = "update venta set id_estado = 5 where id_venta =:id",nativeQuery = true)
+    @Query(value = "update venta set id_estado = 5 where id_venta =:id", nativeQuery = true)
     int anular(@Param("id") Long id);
-    
-    @Query(value = "select max(correlativo_comprobante) from venta where id_tipo_comprobante_sunat =:id", nativeQuery = true)
+
+    @Query(value = "select isnull(max(correlativo_comprobante), 0) from venta where id_tipo_comprobante_sunat =:id", nativeQuery = true)
     int correlativo(@Param("id") Long id);
 
 }
